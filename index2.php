8ac97ce443f87342138273dafbeca1fa9b6ea5c0
jaime <cljrami@gmail.com>
2024-02-08 10:11:30 -0300

    cambios

--- index2.php
+++ index2.php
@@ -1,7 +1,20 @@
-<?php
-$passwordUser = "olson";
-$user = "olson";
-$comando = "\$Password = ConvertTo-SecureString -AsPlainText '$passwordUser' -Force; \$Credential = New-Object System.Management.Automation.PSCredential -ArgumentList '$user', \$Password; Enter-PSSession -ComputerName 192.168.5.125 -Credential \$Credential";
-$output = shell_exec("powershell.exe \"$comando\"");
-echo "
-<pre>$output</pre>";
+<html>
+
+<head>
+    <title>Control MAquina Virtual</title>
+</head>
+
+<body>
+    <h1>Cambio contraseña Usuario Maquina Virtual</h1>
+    <form action="script/proccess.php" method="post">
+        <p>Ingrese la dirección IP de la máquina :</p>
+        <input type="text" name="ip" required>
+        <p>Ingrese el nombre de usuario local:</p>
+        <input type="text" name="username" required>
+        <p>Ingrese la nueva contraseña:</p>
+        <input type="password" name="password" required>
+        <p><input type="submit" value="Cambiar"></p>
+    </form>
+</body>
+
+</html>
---
+++ script/proccess.php
@@ -0,0 +1,24 @@
+<?php
+// Recibir los datos del formulario html
+$ip = $_POST["ip"]; // La dirección IP de la máquina remota
+$username = $_POST["username"]; // El nombre de usuario local
+$password = $_POST["password"]; // La nueva contraseña
+
+// Escapar los argumentos del shell
+$ip = escapeshellarg($ip);
+$username = escapeshellarg($username);
+$password = escapeshellarg($password);
+
+// Construir el comando de powershell
+$command = "powershell -Command \"";
+$command .= "$cred = Get-Credential; "; // Pedir las credenciales del administrador
+$command .= "$session = New-PSSession -ComputerName $ip -Credential $cred; "; // Crear la sesión remota
+$command .= "Invoke-Command -Session $session -ScriptBlock { Set-LocalUser -Name $username -Password $password }; "; // Cambiar la contraseña de usuario
+$command .= "Remove-PSSession -Session $session; "; // Cerrar la sesión remota
+$command .= "\"";
+
+// Ejecutar el comando de powershell y obtener la salida
+$output = shell_exec($command);
+
+// Mostrar la salida
+echo $output;
